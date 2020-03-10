39c94e0ca57d4209952f7430edd7f036cb38b9d2
shuotang642 <56456566+shuotang642@users.noreply.github.com>
2020-03-10 18:35:16 -0400

    New

--- API/Market/app.py
+++ API/Market/app.py
@@ -1,11 +1,14 @@
 import psycopg2
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 
 
 app = Flask(__name__)
+CORS(app)
 
-conn = psycopg2.connect(host='localhost',user='mahesh1',password='mahesh',dbname='marketing_db')
+# conn = psycopg2.connect(host='localhost',user='mahesh1',password='mahesh',dbname='marketing_db')
+conn = psycopg2.connect(host='localhost',user='postgres',password='940818',dbname='marketing_db')
 cur = conn.cursor()
 
 
@@ -19,6 +22,7 @@ def welcome():
         f"n=DJI,FTSE,GSPC,N225,HSI"
         f"<br>   </br>"
         f"/api/v1.0/epidemic/ebola<br/>"
+        f"/api/v1.0/epidemic/corona<br/>"
     )
 
 @app.route("/api/v1.0/index/<index>")
@@ -39,7 +43,7 @@ def market(index):
 
 
 @app.route("/api/v1.0/epidemic/ebola")
-def epidemic():
+def epidemic_e():
     try:
         cur.execute('SELECT * FROM ebola_epidemic')
         values1 = cur.fetchall()
@@ -54,8 +58,20 @@ def epidemic():
         return (f"<h2>An error occured</h2>")
 
 
-            
+@app.route("/api/v1.0/epidemic/corona")
+def epidemic_c():
+    try:
+        cur.execute('SELECT * FROM corona_epidemic')
+        values1 = cur.fetchall()
+
+        if values1 != []:
+           return (jsonify(values1))
+        else:
+            return ("<h3> No row found for epidemic</h3>")   
+
+    except TypeError :
+        print("I am here")           
         
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=False)
